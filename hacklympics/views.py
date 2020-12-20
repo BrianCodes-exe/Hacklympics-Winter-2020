1c832a2e203089dc12a9e4bdd51eb0d64693962e
Brian Kim <74525516+BrianCodes-exe@users.noreply.github.com>
2020-12-19 16:10:06 -0800

    Update views.py

--- hacklympics/views.py
+++ hacklympics/views.py
@@ -1,38 +1,9 @@
 from hacklympics import app
-from flask import render_template, request, redirect
-from pyowm import OWM
+from hacklympics.pyclass.WeatherAPI import WeatherAPI
+from hacklympics.pyclass.NewsAPI import NewsAPI
+from flask import render_template, request, redirect, url_for
 import datetime
 
-class WeatherAPI:
-	def __init__(self, city):
-		self.city = city
-		self.owm = OWM("1ec584803deffede068b569491d027ef")
-		self.mgr = self.owm.weather_manager()
-		self.observation = self.mgr.weather_at_place(city)
-		self.city_ob = self.observation.to_dict()
-		self.coordinates = self.city_ob['location']['coordinates']
-		self.lat, self.lon = self.coordinates['lat'], self.coordinates['lon']
-		self.city_info = self.mgr.forecast_at_place(city, '3h')
-
-	def get_forecast(self):
-		self.hours = [{}]
-		self.call = self.mgr.one_call(self.lat, self.lon)
-		self.hourly_forecast = self.call.forecast_hourly
-
-		self.forecast = []
-
-		for data in self.hourly_forecast:
-			d = {
-			'date': datetime.datetime.fromtimestamp(int(data.reference_time())).strftime('%d'),
-			'hour': datetime.datetime.fromtimestamp(int(data.reference_time())).strftime('%H'),
-			'temperature': data.temperature(unit='fahrenheit')['temp'],
-			'feels_like': data.temperature(unit='fahrenheit')['feels_like'],
-			'humidity': data.humidity,
-			'pressure': data.pressure['press'],
-			'wind': data.wnd['speed']
-			}
-			self.forecast.append(d)
-		return self.forecast
 
 @app.route('/')
 def index():
@@ -41,21 +12,48 @@ def index():
 @app.route('/', methods=['post', 'get'])
 def getCommand():
 	user_input = request.form.get('command')
+	
+	# Command for /weather
 	if '/weather' in user_input:
 		if ' ' in user_input:
 			command = '/weather'
 			city = user_input[9:]
-			wapi = WeatherAPI(city)
-			forecast = wapi.get_forecast()
-			showData = True
-			return render_template('index.html', command=command, forecast=forecast, showData=showData)
+			try:
+				wapi = WeatherAPI(city)
+				forecast = wapi.get_forecast()
+				showData = True
+				return render_template('index.html', command=command, forecast=forecast, showData=showData)
+			except:
+				command='cityDNE'
+				return render_template('index.html', command=command)
 		elif user_input == '/weather':
 			command = 'no city'
-
 			return render_template('index.html', command=command)
+	
+	# Command for /news
+	if '/news' in user_input:
+		command = '/news'
+		if ' ' in user_input:
+			keyword = user_input[6:]
+			newsapi = NewsAPI(keyword)
+			articles = newsapi.get_articles()
+
+			if articles == 'None Found':
+				command=articles
+				showData = False
+			else:
+				showData = True
+
+			return render_template('index.html', command=command, articles=articles, keyword=keyword, showData=showData)
+
+		elif user_input == '/news':
+			newsapi = NewsAPI()
+			articles = newsapi.get_top_headlines()
+			showData = True
+			return render_template('index.html', command=command, articles=articles, showData=showData)
+
+	# Invalid command
 	else:
 		command = 'error'
 
 		return render_template('index.html', command=command)
-
-	

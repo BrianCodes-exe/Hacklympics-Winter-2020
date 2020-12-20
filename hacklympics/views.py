af5cf72b461a91964ea2220535dba9a2f5dd014a
MateuszB-hub <65938049+MateuszB-hub@users.noreply.github.com>
2020-12-19 22:37:11 -0800

    final updates from python website storage

--- hacklympics/views.py
+++ hacklympics/views.py
@@ -2,8 +2,11 @@ from hacklympics import app
 from hacklympics.pyclass.WeatherAPI import WeatherAPI
 from hacklympics.pyclass.NewsAPI import NewsAPI
 from flask import render_template, request, redirect, url_for
-import datetime
+import os
+import time
 
+os.environ["TZ"] = 	"America/Los_Angeles"
+time.tzset()
 
 @app.route('/')
 def index():
@@ -12,36 +15,36 @@ def index():
 @app.route('/', methods=['post', 'get'])
 def getCommand():
 	user_input = request.form.get('command')
-	
+
 	# Command for /weather
 	if '/weather' in user_input:
 		if ' ' in user_input:
 			command = '/weather'
 			city = user_input[9:]
-	
-			wapi = WeatherAPI(city)
-			forecast = wapi.get_forecast()
-			return render_template('index.html', command=command, forecast=forecast, city=city.title())
-		
+			try:
+				wapi = WeatherAPI(city)
+				forecast = wapi.get_forecast()
+				return render_template('index.html', command=command, forecast=forecast, city=city.title())
+			except:
+				command="cityDNE"
+				return render_template('index.html', command=command, city=city.title())
 		elif user_input == '/weather':
 			command = 'no city'
 			return render_template('index.html', command=command)
-	
+
 	# Command for /news
 	if '/news' in user_input:
 		command = '/news'
 		if ' ' in user_input:
 			keyword = user_input[6:]
-			newsapi = NewsAPI(keyword)
-			articles = newsapi.get_articles()
-
-			if articles == 'None Found':
-				command=articles
-				showData = False
-			else:
-				showData = True
-
-			return render_template('index.html', command=command, articles=articles, keyword=keyword, showData=showData)
+			try:
+				newsapi = NewsAPI(keyword)
+				articles = newsapi.get_articles()
+				return render_template('index.html', command=command, articles=articles, keyword=keyword)
+			except:
+				command = 'None Found'
+				articles = None
+				return render_template('index.html', command=command, keyword=keyword)
 
 		elif user_input == '/news':
 			newsapi = NewsAPI()
